71982528a1ff9e58137f0452576b36a4bb898b5a
Anthony Augustus Storti <anthony.storti@uvm.edu>
2021-03-14 22:56:53 +0000

    Upload New File

---
+++ System.py
@@ -0,0 +1,45 @@
+from Organization import Organization as org
+from typing import List
+
+
+class System:
+
+    def __init__(self, name: str, type: str, access_level: int, license_count: int, organizations: List[org]):
+        self._name = name
+        self._type = type
+        self._access_level = access_level
+        self._license_count = license_count
+        self._organizations = organizations
+
+    def get_system_name(self) -> str:
+        return self._name
+
+    def get_system_type(self) -> str:
+        return self._type
+
+    def get_system_access_level(self) -> int:
+        return self._access_level
+
+    def get_system_license_count(self) -> int:
+        return self._license_count
+
+    def get_system_organization(self) -> List[org]:
+        return self._organizations
+
+    def set_system_name(self, name: str):
+        self._name = name
+
+    def set_system_type(self, type: str):
+        self._type = type
+
+    def set_system_access_level(self, access_level: int):
+        self._access_level = access_level
+
+    def set_system_license_count(self, license_count: int):
+        self._license_count = license_count
+
+    def add_system_organization(self, organizations: org):
+        self._organizations.append(organizations)
+
+    def remove_system_organization(self, organizations: org):
+        self._organizations.remove(organizations)
