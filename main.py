779b4af3da75d850cdfc79d33b6efddbf3e8dfce
anthony's xps <anthony.storti@uvm.edu>
2021-04-18 21:59:57 -0400

    made main a little more realistic and readable

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import main_helper as helper
 divisions = ["accounting", "sales", "marketing", "shipping", "executive", "management", "BOD"]
 
 org1 = org("Dunder Mifflin", "Scranton, PA", "Paper Sales", divisions)
-org2 = org("Nakatomi Corporation", "Toyko Japan", "Investments", divisions)
 
 # get 7 system objects into a list
 sys1 = sys("", "", 0, 0, "", list())
@@ -26,37 +25,37 @@ helper.init_system(org1, sys_list, sys1)
 sys8 = sys("", "", 0, 0, "", list())
 helper.init_system(org1, sys_list, sys8, "accounting")
 
-
+print(f'systems created for {org1.get_org_name()}:')
 for s in sys_list:
     print(s.get_system_name())
 
-
-sys8.get_system_type()
-
-sys1.get_username_list()
-
-sys1.set_organization(org1.get_org_name())
-
 usr1 = usr("Michal Scott", "Manager", 2, "Dunder Mifflin", True, list(), sys_list)
-
-usr1.allocate_system("accounting")
 
 usr2 = usr("Jim Halpert", "Sales Associate", 2, "Dunder Mifflin", True, list(), sys_list)
 
-org2.add_user(usr2)
-
-usr2.allocate_system("accounting")
-
-print(sys1.get_username_list())
-
 usr3 = usr("Dwight Shrute", "Assistant to the Regional Manager", 2, "Dunder Mifflin", True, list(), sys_list)
 
+
+print(usr1.get_user_name())
+usr1.allocate_system("accounting")
+
+print(usr2.get_user_name())
+usr2.allocate_system("accounting")
+
+print(usr3.get_user_name())
 usr3.allocate_system("management")
 
-print(sys6.get_username_list())
+sys1.set_organization(org1.get_org_name())
 
+print(f"{sys8.get_system_name()} type is {sys8.get_system_type()}")
+
+print(f"System 1 users: {sys1.get_username_list()}")
+
+print(f"System 6 users: {sys6.get_username_list()}")
+
+print(usr3.get_user_name())
 usr3.terminate()
 
-org1.get_org_name()
+
 
 
